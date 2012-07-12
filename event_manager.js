32ab2a741630c026623d2637955558b57692dfb6
Benjamin Wheeler <bwheeler@webassign.net>
2012-07-12 13:24:10 -0400

    Added a basic event manager

---
+++ event_manager.js
@@ -0,0 +1,20 @@
+define([
+],
+function() {
+    function on(events, selector, data, handler){
+        return dispatcher.on(events, selector, data, handler);
+    }
+
+    function trigger(eventType, extraParameters){
+        return dispatcher.triggerHandler(eventType, extraParameters);
+    }
+
+    EventManager = function() {
+        obj.on      = on;
+        obj.trigger = trigger;
+    }
+
+    var dispatcher = $({});
+
+    return EventManager;
+})();
